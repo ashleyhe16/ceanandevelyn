b6af37b59a9595edef7de445e2d3f891ead834ae
Ashley <ashley@ashleys-laptop.lan>
2024-06-24 21:54:09 -0400

    initial changes

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Footer = (props) => {
+  return <footer className="footer">Made with love for Cean & Evelyn</footer>;
+};
---
+++ src/components/Layout.js
@@ -0,0 +1,4 @@
+import React from "react";
+// import "../styles/global.scss";
+
+export default ({ children }) => <div>{children}</div>;
